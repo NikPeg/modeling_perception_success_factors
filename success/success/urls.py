6eaf9cf4a14d8e198fab8732aea04a4b42eee5f4
NikPeg <peganov.nik@gmail.com>
2024-03-18 23:46:28 +0300

    About page

--- success/success/urls.py
+++ success/success/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, re_path
-from .views import home_view
+from .views import home_view, about_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', home_view, name='home'),
+    path("", home_view, name="home"),
+    path("about/", about_view, name='about'),
 ]

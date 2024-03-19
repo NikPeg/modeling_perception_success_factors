51bde7925358d1fd42d2a2031b278931f275bf85
NikPeg <peganov.nik@gmail.com>
2024-03-19 23:15:10 +0300

    js and css

---
+++ success/success/static/script.js
@@ -0,0 +1,9 @@
+// Функция для отображения всплывающего окна
+function showLoginPopup() {
+    document.getElementById("loginPopup").style.display = "block";
+}
+
+// Функция для скрытия всплывающего окна
+function hideLoginPopup() {
+    document.getElementById("loginPopup").style.display = "none";
+}

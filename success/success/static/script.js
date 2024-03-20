37487585fad78c3b4861b997950a816f2d4514e5
NikPeg <peganov.nik@gmail.com>
2024-03-20 13:52:07 +0300

    Add register

--- success/success/static/script.js
+++ success/success/static/script.js
@@ -6,3 +6,11 @@ function hideLoginPopup() {
     document.getElementById("backgroundOverlay").style.display = "none";
     document.getElementById("loginPopup").style.display = "none";
 }
+function showRegisterPopup() {
+    document.getElementById("backgroundOverlay").style.display = "block";
+    document.getElementById("registerPopup").style.display = "block";
+}
+function hideRegisterPopup() {
+    document.getElementById("backgroundOverlay").style.display = "none";
+    document.getElementById("registerPopup").style.display = "none";
+}

f114bd8af6a968014e149159d10967f533febb03
NikPeg <peganov.nik@gmail.com>
2024-04-10 14:18:48 +0300

    working login

--- success/success/models.py
+++ success/success/models.py
@@ -5,9 +5,5 @@ from django.db import models
 class User(AbstractUser):
     email = models.EmailField(unique=True)
 
-    # Add any additional fields you need
-    # Example:
-    # profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
-
     def __str__(self):
         return self.username
